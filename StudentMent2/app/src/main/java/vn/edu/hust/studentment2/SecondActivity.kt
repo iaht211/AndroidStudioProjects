cbdb5ce37b3fe5bc341a626ad8ec7eeef921d0b3
iaht211 <nguyenvanthai092003@gmail.com>
2024-11-26 22:18:26 +0700

    student man 2

---
+++ StudentMent2/app/src/main/java/vn/edu/hust/studentment2/SecondActivity.kt
@@ -0,0 +1,21 @@
+package vn.edu.hust.studentment2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        // TODO: Lay du lieu dinh kem khi mo activity
+        val value1 = intent.getIntExtra("param3", 0)
+        val value2 = intent.getDoubleExtra("param2", 0.0)
+        val value3 = intent.getStringExtra("param1")
+
+        val textContent = findViewById<TextView>(R.id.text_content)
+        textContent.text = "$value1 - $value2 - $value3"
+
+    }
+}
